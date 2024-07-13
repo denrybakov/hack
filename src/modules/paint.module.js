d311db3d0cdda8bb79130745f19bfc6737398c66
denrybakov <rybakovdev@yandex.ru>
2024-07-13 22:58:50 +0300

    fixed overflow hidden

--- src/modules/paint.module.js
+++ src/modules/paint.module.js
@@ -35,6 +35,7 @@ export class PaintModule extends Module {
     this.isDrawing = true
     this.ctx.lineWidth = 2
     this.ctx.beginPath()
+
     this.ctx.moveTo(e.offsetX, e.offsetY)
   }
 
@@ -53,10 +54,12 @@ export class PaintModule extends Module {
   #listenerTools = (e) => {
     this.$ulTools.childNodes.forEach(li => li.classList.remove('active'))
     this.ctx.strokeStyle = e.target.className;
+
     if (e.target.classList.contains('clear')) {
       this.ctx.lineWidth = 4
       this.ctx.strokeStyle = 'white';
     }
+
     if (e.target.classList.contains('fill')) {
       if (confirm('Вы уверены что хотите очистить свое произведение искусства?')) {
         this.ctx.clearRect(0, 0, this.$canvas.width, this.$canvas.height)
@@ -67,13 +70,24 @@ export class PaintModule extends Module {
         this.ctx.strokeStyle = 'black'
       }
     }
+
+    if (e.target.classList.contains('none')) {
+      this.$ulTools.childNodes[0].classList.add('active')
+      this.ctx.strokeStyle = 'black'
+    }
+
+    if (e.target.classList.contains('tools')) {
+      this.$ulTools.childNodes[0].classList.add('active')
+      this.ctx.strokeStyle = 'black'
+    }
+
     e.target.classList.add('active')
   }
 
   #downloadCanvas = () => {
     this.$linkDownload = document.createElement('a')
-    this.$linkDownload.download = `${Date.now()}-MyPicture.png`
-    this.$linkDownload.href = this.$canvas.toDataURL('image/png')
+    this.$linkDownload.download = `${Date.now()}-MyPicture.jpeg`
+    this.$linkDownload.href = this.$canvas.toDataURL('image/jpeg')
     this.$linkDownload.click()
   }
 
@@ -82,6 +96,7 @@ export class PaintModule extends Module {
     this.$label = addElemNode('div', 'paint__label', ' Твори Созидай Улучшай Изменяй ')
     this.$canvas = addElemNode('canvas', 'paint__canvas', '')
     this.ctx = this.$canvas.getContext('2d')
+    this.ctx.fillStyle = 'white'
 
     this.$btnDownload = addElemNode('button', 'paint__download', 'Скачать искусство')
     this.$btnClose = addElemNode('span', 'paint__close', '')
