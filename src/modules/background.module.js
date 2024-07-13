7f13b178fd28009908b280e6b948979772e922e7
denrybakov <rybakovdev@yandex.ru>
2024-07-14 01:41:22 +0300

    add new feature download img and fixed background

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -4,11 +4,18 @@ import { random } from '../utils';
 export class BackgroundModule extends Module {
   constructor(type, text) {
     super(type, text)
+    this.arrBackgrounds = ['../assets/fon1.jpg', '../assets/fon2.jpg', '../assets/fon3.jpg', '../assets/fon4.jpg', '../assets/fon5.jpg']
   }
 
   trigger() {
-    // document.body.innerHTML = ''
-    document.querySelector('body').style.backgroundColor = `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+    }
+    this.intervalId = setInterval(this.#getRandomBackground, 3000);
   }
 
+  #getRandomBackground = () => {
+    document.querySelector('body').style.background = `url('${this.arrBackgrounds[random(0, this.arrBackgrounds.length)]}') no-repeat center / cover`;
+  }
 }
+
