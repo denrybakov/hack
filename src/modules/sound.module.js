95cfc0cdaf51f0471de096391b08093947fdbe5b
Alexander Belov <alexander1993belov@yandex.ru>
2024-07-13 19:40:28 +0300

    first commit

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,5 +1,7 @@
 import { Module } from '../core/module'
 
+
+
 export class SoundModule extends Module {
   constructor(type, text) {
     super(type, text)
@@ -7,6 +9,15 @@ export class SoundModule extends Module {
 
 
   trigger() {
-    alert('Метод создания случайного звука !')
+    const sounds = [
+      '../assets/Sound_04572.mp3',
+      '../assets/Sound_04563.wav',
+  ];
+
+  const randomIndex = Math.floor(Math.random() * sounds.length);
+  const randomSound = sounds[randomIndex];
+
+  const audio = new Audio(randomSound);
+    audio.play();
   }
 }
