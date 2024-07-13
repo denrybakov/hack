27e3726075c66f8a9f03a4652c2a9977e8341e16
MazaevKA <mazaj.ru@mail.ru>
2024-07-13 15:53:17 +0500

    shape is ready

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,4 +1,5 @@
-import { Module } from '../core/module'
+import { Module } from '../core/module';
+import {random} from '../utils';
 
 export class ShapeModule extends Module {
   constructor(type, text) {
@@ -7,6 +8,25 @@ export class ShapeModule extends Module {
 
 
   trigger() {
-    alert('Метод создания фигуры !')
+    const shape = document.createElement('div');
+    shape.classList.add(`figure-${random(1, 5)}`);
+    shape.style.width = `${random(100, 300)}px`;
+    shape.style.height = `${random(100, 300)}px`;
+    shape.style.position = 'absolute';
+    shape.style.top = `${random(parseInt(shape.style.height), window.innerHeight - parseInt(shape.style.height))}px`;
+    shape.style.left = `${random(parseInt(shape.style.width), window.innerWidth - parseInt(shape.style.width))}px`;
+    shape.style.background = `rgb(${random(0, 225)},${random(0, 255)},${random(0, 255)})`;
+    document.querySelector('body').append(shape);
+
+    setTimeout(() => {
+      shape.remove();
+    }, 10000);
+
+    setInterval(moveBlock, 1000);
+    function moveBlock() {
+      shape.style.top = `${(random(parseInt(shape.style.height), window.innerHeight - parseInt(shape.style.height)))}px`;
+      shape.style.left = `${(random(parseInt(shape.style.width), window.innerWidth - parseInt(shape.style.width)))}px`;
+    }
   }
 }
+
