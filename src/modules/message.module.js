324ec2729da415050d052df1453e18ca42a18c78
Denis Bekkulov <dionisvegan@icloud.com>
2024-07-14 15:15:00 +0300

    add random messages on the screen from API

--- src/modules/message.module.js
+++ src/modules/message.module.js
@@ -1,12 +1,33 @@
-import { Module } from '../core/module'
+import { Module } from '../core/module';
+import { random } from '../utils';
 
 export class MessageModule extends Module {
-  constructor(type, text) {
-    super(type, text)
-  }
+	constructor(type, text) {
+		super(type, text);
+		this.el = document.querySelector('body');
+		this.TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
+	}
 
+	trigger() {
+		this.randomID = random(1, 200);
+		this.fetchingTodos();
+	}
+	fetchingTodos() {
+		fetch(`${this.TODOS_URL}/${this.randomID}`)
+			.then(response => response.json())
+			.then(response => this.createMessage(response))
+			.catch(error => {
+				console.error(error);
+			})
+			.finally(() => {
+				console.log('message on the screen!');
+			});
+	}
 
-  trigger() {
-    alert('Метод создания сообщения !')
-  }
+	createMessage(response) {
+		this.messagePar = document.createElement('p');
+		this.messagePar.className = 'message-par';
+		this.messagePar.textContent = `${response.title}`;
+		this.el.append(this.messagePar);
+	}
 }
