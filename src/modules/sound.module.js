6c3a8e3920009a52d471ba9fb195262b5d57968f
denrybakov <rybakovdev@yandex.ru>
2024-07-14 22:18:24 +0300

    sound

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,54 +1,55 @@
-import { Module } from '../core/module'
-
-
+import { Module } from '../core/module';
 
 export class SoundModule extends Module {
-  
-
   constructor(type, text) {
-    super(type, text)
+    super(type, text);
+    this.playerBlock = null;
+    this.player = null;
   }
 
+  #stopCurrentTrack() {
+    if (this.player) {
+      this.player.pause();
+      this.player.src = '';
+      this.player.load();
+    }
+    if (this.playerBlock) {
+      this.playerBlock.remove();
+      this.playerBlock = null;
+    }
+  }
 
   #createPlayer(src) {
+    this.#stopCurrentTrack(); // Остановить текущий трек перед созданием нового
+
     this.playerBlock = document.createElement('div');
     this.playerBlock.className = 'total';
     this.player = document.createElement('video');
     this.player.name = 'media';
     this.player.setAttribute('controls', '');
     this.player.setAttribute('autoplay', '');
-    this.source = document.createElement('source');
-    this.source.src = src;
-    this.source.type = 'audio/mpeg';
-    this.player.append(this.source);
+    const source = document.createElement('source');
+    source.src = src;
+    source.type = 'audio/mpeg';
+    this.player.append(source);
     this.playerBlock.append(this.player);
     document.body.append(this.playerBlock);
-
   }
 
   trigger() {
+    const sounds = [
+      './assets/sound1.mp3',
+      './assets/sound2.mp3',
+      './assets/sound3.mp3',
+      './assets/sound4.mp3',
+      './assets/sound5.mp3',
+      './assets/sound6.mp3',
+      './assets/sound7.mp3'
+    ];
 
-        this.sounds = [
-          './assets/sound1.mp3',
-          './assets/sound2.mp3',
-          './assets/sound3.mp3',
-          './assets/sound4.mp3',
-          './assets/sound5.mp3',
-          './assets/sound6.mp3',
-          './assets/sound7.mp3'
-      ];
-    
+    const randomIndex = Math.floor(Math.random() * sounds.length);
+    const randomSound = sounds[randomIndex];
 
-      this.randomIndex = Math.floor(Math.random() * this.sounds.length);
-      this.randomSound = this.sounds[this.randomIndex];
-  
-      this.audio = new Audio(this.randomSound);
-     
-
-      this.#createPlayer(this.audio.src);
-
-
+    this.#createPlayer(randomSound);
   }
-
-  
 }
