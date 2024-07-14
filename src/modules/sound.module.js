1d7f583a2cd2b65df5989cb79ff7aecb05503b5d
Vyacheslav <sbhlrsa@gmail.com>
2024-07-14 22:52:05 +0400

    add player and random play music

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,66 +1,54 @@
 import { Module } from '../core/module'
-import { random } from '../utils'
+
 
 
 export class SoundModule extends Module {
+  
+
   constructor(type, text) {
     super(type, text)
   }
 
 
+  #createPlayer(src) {
+    this.playerBlock = document.createElement('div');
+    this.playerBlock.className = 'total';
+    this.player = document.createElement('video');
+    this.player.name = 'media';
+    this.player.setAttribute('controls', '');
+    this.player.setAttribute('autoplay', '');
+    this.source = document.createElement('source');
+    this.source.src = src;
+    this.source.type = 'audio/mpeg';
+    this.player.append(this.source);
+    this.playerBlock.append(this.player);
+    document.body.append(this.playerBlock);
+
+  }
+
   trigger() {
 
-
-      // var audio1 = new Audio()
-      // audio1.src = '../sound/Sound_04572.mp3'
-      // audio1.autoplay = true
-      
-
-
-        // var audio = new Audio(); // Создаём новый элемент Audio
-        // audio.src = '../sound/Sound_04572.mp3'; // Указываем путь к звуку "клика"
-        // audio.autoplay = true; // Автоматически запускаем
-      
-      // const audio = new Audio('../../../../sound/Sound_04572.mp3')
-      // audio.play() 
-
-        // const player = document.createElement('audio')
-        // player.src = '../sound/Sound_04572.mp3'
-        // player.preload = "auto";
-        // player.autoplay = true
-        // player.play()
-
-        const sounds = [
+        this.sounds = [
           './assets/sound1.mp3',
           './assets/sound2.mp3',
-          './assets/sound3.mp3'
+          './assets/sound3.mp3',
+          './assets/sound4.mp3',
+          './assets/sound5.mp3',
+          './assets/sound6.mp3',
+          './assets/sound7.mp3'
       ];
-  
-      const randomIndex = Math.floor(Math.random() * sounds.length);
-      const randomSound = sounds[randomIndex];
-  
-      const audio = new Audio(randomSound);
-      audio.play();
-   
-
-      //   // player.play()
-      // soundClick()
-    // this.soundArray = []
-    // this.myAudio1 = new Audio('../sound/Sound_04563.wav')
-    // this.myAudio1.autoplay = true
-    // // this.myAudio2 = new Audio('../sound/Sound_04563.wav')
-    // // this.myAudio2.autoplay = true
-    //   console.log(this.myAudio1)
-    //   this.myAudio1.play()
-  
-      
-
-    // this.soundArray. push(this.myAudio1, this.myAudio2) 
-    // console.log(this.soundArray)
-    // // console.log(this.soundArray[0].play())
     
-    // this.soundArray[random(0, this.soundArray.length)].play()
+
+      this.randomIndex = Math.floor(Math.random() * this.sounds.length);
+      this.randomSound = this.sounds[this.randomIndex];
+  
+      this.audio = new Audio(this.randomSound);
+     
+
+      this.#createPlayer(this.audio.src);
 
 
   }
+
+  
 }
