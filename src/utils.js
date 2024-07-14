57608b0374c6472865cf67825bb7ba321203b222
denrybakov <rybakovdev@yandex.ru>
2024-07-14 21:41:36 +0300

    fixed err utils

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,8 @@
 export const TOKEN_TINY = '0ZdVBZOeoqMFK66lFBwKPMDN9O9gXdS9SMWgRvw284MINa5lulxK0MgtGGAk'
 
 export function random(min, max) {
-	return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 }
-
-export const addElemNode = (elem, classElem, textContent) =>
-	new DOMParser().parseFromString(
-		`<${elem} class=${classElem}>${textContent}</${elem}>`,
-		'text/html'
-	).body.firstChild;
 
 export const addElemNode = (elem, classElem = '', textContent = '') =>
   new DOMParser()
